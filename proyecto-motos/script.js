5c636b999a4cfbd26682fbfb5b00b932080afb42
YaelCasti <yaelgaspar@hotmail.com>
2025-03-10 10:54:56 -0600

    script.js

--- proyecto-motos/script.js
+++ proyecto-motos/script.js
@@ -21,4 +21,30 @@ $(document).ready(function(){
     $('#ir-arriba').click(function(){
         $('html, body').animate({scrollTop: 0}, 'slow');
     });
+    
+    // Validar formulario de contacto
+    $('#formulario-contacto').on('submit', function(event) {
+        // Prevenir el envío del formulario por defecto
+        event.preventDefault();
+
+        // Obtener los valores de los campos
+        const nombre = $('#nombre').val();
+        const email = $('#email').val();
+        const mensaje = $('#mensaje').val();
+
+        // Verificar si los campos están vacíos
+        if (nombre === '' || email === '' || mensaje === '') {
+            alert('Por favor, completa todos los campos antes de enviar.');
+        } else if (!validarEmail(email)) {
+            alert('Por favor, introduce un correo electrónico válido.');
+        } else {
+            alert('Formulario enviado con éxito.');
+         }
+    });
+
+    // Función para validar el formato del email
+    function validarEmail(email) {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    }
 });
